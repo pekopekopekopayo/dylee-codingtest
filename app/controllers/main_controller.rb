7f6cb0269fa1c97d33374afd644172ed4e6a3085
pekopekopekopayo <dlehddus1285@gmail.com>
2023-07-26 12:19:56 +0900

    skip_before_action 잘못된 선언 수정

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@
 
 class MainController < ApplicationController
   # Task1: IP가 차단 상태여도 landing_page페이지는 볼수있게한다.
-  skip_before_action :block_ips, only: :index
+  skip_before_action :block_ips, only: :landing_page
 
   def landing_page
     render plain: 'Welcome Page', status: :ok
